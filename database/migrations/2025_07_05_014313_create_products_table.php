9a4f04639058afaef9564a2968f59f20dd121180
HuynhNgocPhu <phuhnps38753@gmail.com>
2025-07-20 23:40:24 +0700

    [creat] update loi and creat file seeders/termsAndConditionsSeeder.php

--- database/migrations/2025_07_05_014313_create_products_table.php
+++ database/migrations/2025_07_05_014313_create_products_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-             $table->id();
+       Schema::table('products', function (Blueprint $table) {
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('code')->unique();
@@ -27,7 +26,6 @@ return new class extends Migration
             $table->morphs('created_by');
             $table->morphs('updated_by');
             $table->softDeletes();
-            $table->timestamps();
         });
     }
 
@@ -36,6 +34,21 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        // Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+              $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('code')->unique();
+            $table->foreignId('brand_id')->nullable()->constrained('brands')->onDelete('cascade');
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(1);
+            $table->tinyInteger('type')->comment(ProductTypeEnum::class);
+            $table->string('primary_image')->nullable();
+            $table->json('media')->nullable();
+            $table->json('suggested_relationships')->nullable();
+            $table->morphs('created_by');
+            $table->morphs('updated_by');
+            $table->softDeletes();
+        });
     }
 };
